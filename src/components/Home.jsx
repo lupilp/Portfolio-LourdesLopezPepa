8134c1167d65a97ef1c37c38482496fe39fd2498
lupilp <lupilp97@gmail.com>
2022-12-14 12:45:21 -0300

    avance

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,15 @@ import foto from "../images/foto.jpg";
 
 function Home() {
   return (
-    <div>
-      <div>
+    <div className={style.contGeneral}>
+      <div className={style.contMenu}>
         <img src={foto} alt="foto-perfil" className={style.foto} />
-        <div>Home</div>
-        <div>Sobre Mi</div>
-        <div>Mis Proyectos</div>
-        <div>Contacto</div>
+        <div className={style.todosItems}>
+          <div className={style.item}>Home</div>
+          <div className={style.item}>Sobre Mi</div>
+          <div className={style.item}>Mis Proyectos</div>
+          <div className={style.item}>Contacto</div>
+        </div>
       </div>
     </div>
   );
