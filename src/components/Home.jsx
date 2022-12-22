2e9b42ba35f35f828af10b67d7e4987228ef1e45
lupilp <lupilp97@gmail.com>
2022-12-22 16:15:30 -0300

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../style/Home.module.css";
 import foto from "../images/foto.jpg";
 import SobreMi from "./SobreMi";
 import HomeCont from "./HomeCont";
 import Proyectos from "./Proyectos";
 import Contacto from "./Contacto";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function Home() {
+  const [isOpen, setisOpen] = useState(false);
+
+  const handleClick = () => {
+    setisOpen(!isOpen);
+  };
+
   return (
     <div className={style.contGeneral}>
-      <div className={style.contMenu}>
-        <img src={foto} alt="foto-perfil" className={style.foto} />
-        <div className={style.todosItems}>
-          <a href="#home" className={style.item}>
-            Home
-          </a>
+      <div className={`${style.menuAbierto} ${!isOpen && style.menuCerrado}`}>
+        {isOpen ? (
+          <>
+            <AiOutlineClose onClick={handleClick} fill="#fff" />
+            <img src={foto} alt="foto-perfil" className={style.foto} />
+            <div className={style.todosItems}>
+              <a href="#home" className={style.item}>
+                Home
+              </a>
 
-          <a href="#sobreMi" className={style.item}>
-            Sobre Mi
-          </a>
+              <a href="#sobreMi" className={style.item}>
+                Sobre Mi
+              </a>
 
-          <a href="#proyectos" className={style.item}>
-            Mis Proyectos
-          </a>
+              <a href="#proyectos" className={style.item}>
+                Mis Proyectos
+              </a>
 
-          <a href="#contacto" className={style.item}>
-            Contacto
-          </a>
-        </div>
+              <a href="#contacto" className={style.item}>
+                Contacto
+              </a>
+            </div>
+          </>
+        ) : (
+          <AiOutlineMenu onClick={handleClick} fill="#fff" />
+        )}
       </div>
 
       <div className={style.contenedor}>
